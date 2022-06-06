626a174ff7cd2ca60191bb2f2d31853d73c4be8f
Vinicius Silvestre <vinicius.moreira134@yahoo.com>
2022-06-06 12:49:25 -0300

    Add aulas de Java pelo STS

---
+++ Aulas Java/src/br/com/generation/aula01/ExemploVariavel01.java
@@ -0,0 +1,21 @@
+package br.com.generation.aula01;
+
+public class ExemploVariavel01 {
+
+	public static void main(String[] args) {
+		
+		int var1, var2;
+	
+		var1=1000;
+		
+		System.out.printf("Valor da Variavel 1: %d\n",var1);
+		System.out.println("Valor da Variavel 1: "+var1);
+		
+		var2 = var1 / 2;
+				
+		System.out.println("Valor da Variavel 2: "+var2);
+				
+		
+	}
+
+}
